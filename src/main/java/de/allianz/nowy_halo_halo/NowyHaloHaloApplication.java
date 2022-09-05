942b43d3ac9c4af411d008f09bc155831a4dcf53
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-09-05 13:36:36 +0200

    Create gradle project

---
+++ src/main/java/de/allianz/nowy_halo_halo/NowyHaloHaloApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.nowy_halo_halo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowyHaloHaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowyHaloHaloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/nowy_halo_halo/NowyHaloHaloApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.nowy_halo_halo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowyHaloHaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
